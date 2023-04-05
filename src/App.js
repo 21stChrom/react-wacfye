1de4570024b232817c8713c714053c8f8a6ecd24
21stChrom <81393334+21stChrom@users.noreply.github.com>
2023-04-05 19:20:43 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,117 @@
+
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Tesseract from 'tesseract.js';
+import QRCode from 'qrcode.react';
+import { saveAs } from 'file-saver';
+
+function App() {
+  const [imageUrl, setImageUrl] = useState("");
+    const [transcription, setTranscription] = useState("");
+      const [qrValue, setQrValue] = useState("");
+
+        const handleFileSelect = (e) => {
+            const file = e.target.files[0];
+                const reader = new FileReader();
+                    reader.readAsDataURL(file);
+                        reader.onload = () => {
+                              setImageUrl(reader.result);
+                                  };
+                                    };
+
+                                      const handleTranscribe = async () => {
+                                          const { data: { text } } = await Tesseract.recognize(imageUrl, 'eng');
+                                              setTranscription(text);
+                                                  setQrValue(text);
+                                                    };
+
+                                                      return (
+                                                          <div>
+                                                                <input type="file" onChange={handleFileSelect} />
+                                                                      <button onClick={handleTranscribe}>Transcribe</button>
+                                                                            <QRCode value={qrValue} />
+                                                                                  <p>{transcription}</p>
+
+                                                                                    const handleDownload = () => {
+                                                                                        const canvas = document.getElementById('qrcode');
+                                                                                            const dataURL = canvas.toDataURL('image/png');
+                                                                                                saveAs(dataURL, 'qrcode.png');
+                                                                                                  };
+
+                                                                                                    return (
+                                                                                                        <div>
+                                                                                                              <h1>Transcribe Image Text</h1>
+                                                                                                                    <label htmlFor="image-file">Select an image file to transcribe:</label>
+                                                                                                                          <input type="file" id="image-file" accept="image/*" onChange={handleFileSelect} />
+                                                                                                                                {imageUrl && <img src={imageUrl} alt="Selected image" />}
+                                                                                                                                      <button onClick={handleTranscribe}>Transcribe</button>
+                                                                                                                                            <div>
+                                                                                                                                                    <h2>Transcription:</h2>
+                                                                                                                                                            <p>{transcription}</p>
+                                                                                                                                                                    <QRCode id="qrcode" value={qrValue} />
+                                                                                                                                                                            <button onClick={handleDownload}>Download QR Code</button>
+                                                                                                                                                                                  </div>
+                                                                                                                                                                                      </div>
+                                                                                                                                                                                        );
+                                                                                                                                                                                        }
+
+                                                                                                                                                                                        ReactDOM.render(<App />, document.getElementById('root'));
+                                                                                                                                                                                        ```
+
+                                                                                                                                                                                        The changes include:
+
+                                                                                                                                                                                        - Wrapping the JSX code inside a `div` to enclose all the elements.
+                                                                                                                                                                                        - Fixing the JSX syntax by adding closing tags for certain elements.
+                                                                                                                                                                                        - Passing the `transcription` variable to the `onClick` handler of the `Transcribe` button.
+                                                                                                                                                                                        - Adding a `Download QR Code` button to download the QR code as a PNG.
+                                                                                                                                                                                        - Adding a `div` wrapper around the `Transcription` and `QRCode` elements, and changing the `id` of the `QR
+                                                                                                                                                                                        PS I love you. And i asked the Ask AI app to write this for me. Get it for free --> https://get-askai.app
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                        const path = require('path');
+
+                                                                                                                                                                                        module.exports = {
+                                                                                                                                                                                          entry: './src/index.js',
+                                                                                                                                                                                            output: {
+                                                                                                                                                                                                path: path.resolve(__dirname, 'dist/YourPackageBundle'),
+                                                                                                                                                                                                    filename: 'bundle.js',
+                                                                                                                                                                                                        libraryTarget: 'commonjs2'
+                                                                                                                                                                                                          },
+                                                                                                                                                                                                            module: {
+                                                                                                                                                                                                                rules: [
+                                                                                                                                                                                                                      {
+                                                                                                                                                                                                                              test: /\.js$/,
+                                                                                                                                                                                                                                      exclude: /node_modules/,
+                                                                                                                                                                                                                                              use: 'babel-loader'
+                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                        ]
+                                                                                                                                                                                                                                                          },
+                                                                                                                                                                                                                                                            resolve: {
+                                                                                                                                                                                                                                                                extensions: ['.js']
+                                                                                                                                                                                                                                                                  },
+                                                                                                                                                                                                                                                                    externals: {
+                                                                                                                                                                                                                                                                        react: 'commonjs react'
+                                                                                                                                                                                                                                                                          }
+                                                                                                                                                                                                                                                                          };
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                              </div>
+                                                                                                                                                                                                                                                                                );
+                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                
